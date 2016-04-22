5f6f7e2679a4bdf59017d95b0ff13b4ff067617c
David M. Anderson <dma@epsilondelta.us>
2016-04-21 17:02:51 -0700

    Added word counts.
    Also clear textarea on focus

--- public/Assignments/Solutions/3/NumbersAndStrings.js
+++ public/Assignments/Solutions/3/NumbersAndStrings.js
@@ -13,11 +13,13 @@ var count = 0;
 var sum = 0;
 var average;
 var concatenatedString = '';
+var wordCounts = {};
 
 DisplayResults( );
 
 $('#submit').on( 'click', processForm );
 $('#reset').on( 'click', reset );
+$('#user-input').on( 'focus', clearInput );
 
 //=============================================================================
 
@@ -36,7 +38,26 @@ function processForm( evt ) {
 
 function processString( string ) {
     concatenatedString += string;
+    updateWordCounts( string );
     DisplayResults( );
+}
+
+//=============================================================================
+
+function updateWordCounts( string ) {
+    var words = string.split( /\s/ ); //split on whitespace (Could use ' ')
+    words.forEach( function( word ) {
+        word = word.toLowerCase();
+        word = word.replace( /\W/, '' ); //remove non-word characters
+        if ( word.length === 0 ) {
+            return;
+        }
+        if ( wordCounts[ word ] === undefined ) {
+            wordCounts[ word ] = 1;
+        } else {
+            ++wordCounts[ word ];
+        }
+    } );
 }
 
 //=============================================================================
@@ -59,6 +80,7 @@ function DisplayResults( ) {
     displayValue( '#sum', sum );
     displayValue( '#average', average );
     displayValue( '#concatenation', concatenatedString );
+    displayWordCounts( );
 
     //-------------------------------------------------------------------------
 
@@ -73,12 +95,46 @@ function DisplayResults( ) {
 
 //=============================================================================
 
+function displayWordCounts( ) {
+    var words = Object.keys( wordCounts );
+    var tr, td;
+
+    words.sort( function( word1, word2 ) {
+        //Sort in descending order by count
+        return wordCounts[ word2 ] - wordCounts[ word1 ];
+    } );
+
+    $('#word-counts').empty( );
+    words.forEach( function( word ) {
+        tr = $( '<tr>' );
+
+        td = $( '<td>' );
+        td.text( word );
+        tr.append( td );
+
+        td = $( '<td>' );
+        td.text( wordCounts[ word ] );
+        tr.append( td );
+
+        $('#word-counts').append( tr );
+    } );
+}
+
+//=============================================================================
+
 function reset( ) {
     count = 0;
     sum = 0;
     average = undefined;
     concatenatedString = '';
+    wordCounts = {};
     DisplayResults( );
+}
+
+//=============================================================================
+
+function clearInput( ) {
+    $('#user-input').val( '' );
 }
 
 //=============================================================================
